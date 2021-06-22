46e9f5ea5fcb8496b6237619a8463249a79dfcf9
swingcloud <sam@swingcloud.app>
2021-06-22 23:27:51 +0800

    add mock test

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule WeatherApi.MixProject do
       {:finch, "~> 0.7.0"},
       {:jason, "~> 1.2.2"},
       {:bypass, "~> 2.1", only: :test},
-      {:hammox, "~> 0.5", only: :test}
+      {:hammox, "~> 0.5", only: :test},
+      {:mock, "~> 0.3.7", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
